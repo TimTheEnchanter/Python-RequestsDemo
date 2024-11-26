88c62edd5220fe96386d29358d67013faae1d14c
James Parrott <japarrott@shaw.ca>
2024-11-26 12:57:03 -0700

    request headers

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ response = requests.get(
     params={"q": "language:python", "sort": "stars", "order": "desc"},
 )
 
+response2 = requests.get(
+    "https://api.github.com/search/repositories",
+    params={"q": '"real python"'},
+    headers={"Accept": "application/vnd.github.text-match+json"},
+)
+
 json_response = response.json()
 popular_repositories = json_response["items"]
-for repo in popular_repositories[:3]:
+for repo in popular_repositories[:5]:
     print(f"Name: {repo['name']}")
     print(f"Description: {repo['description']}")
     print(f"Stars: {repo['stargazers_count']}")
     print()
+    
+json_response2 = response2.json()
+first_repository = json_response2["items"][0]
+print(first_repository["text_matches"][0]["matches"])
