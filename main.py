0dad01ecc9f052353b8ce22254a566db98623f7e
James Parrott <japarrott@shaw.ca>
2024-11-26 12:18:04 -0700

    access response object to determine success of request

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
 
 response = requests.get("https://api.github.com")
-print(response)
+
+if response.status_code == 200:
+    print("Success!")
+elif response.status_code == 404:
+    print("Not Found.")
