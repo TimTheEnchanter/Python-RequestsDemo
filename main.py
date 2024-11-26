9ca3996e3ebfeab2a71f7577c34059a0da4c5f3b
James Parrott <japarrott@shaw.ca>
2024-11-26 12:18:56 -0700

    instead of printing out a message raise an exception

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ response = requests.get("https://api.github.com")
 if response.status_code == 200:
     print("Success!")
 elif response.status_code == 404:
-    print("Not Found.")
+    raise Exception(f"Request Failed - status code: {response.status_code}")
